e8a0b499a092f62e4f1ab3c468b68f95bf7bc324
dode5656 <dev.dode5656@gmail.com>
2020-03-05 00:05:35 +0400

    Fix needing to restart after changing config.yml

--- src/main/java/io/github/dode5656/rolesync/commands/ReloadCommand.java
+++ src/main/java/io/github/dode5656/rolesync/commands/ReloadCommand.java
@@ -1,6 +1,7 @@
 package io.github.dode5656.rolesync.commands;
 
 import io.github.dode5656.rolesync.RoleSync;
+import io.github.dode5656.rolesync.events.JoinEvent;
 import io.github.dode5656.rolesync.utilities.Message;
 import io.github.dode5656.rolesync.utilities.PluginStatus;
 import net.md_5.bungee.api.CommandSender;
@@ -27,6 +28,9 @@ public class ReloadCommand extends Command {
             if (plugin.getPluginStatus() == PluginStatus.DISABLED) plugin.setPluginStatus(PluginStatus.ENABLED);
             plugin.getConfigChecker().checkDefaults();
             plugin.startBot();
+            plugin.getProxy().getPluginManager().registerCommand(plugin, new SyncCommand(plugin));
+            plugin.getProxy().getPluginManager().registerCommand(plugin, new ReloadCommand(plugin));
+            plugin.getProxy().getPluginManager().registerListener(plugin, new JoinEvent(plugin));
         } catch (Exception e) {
             plugin.getLogger().log(Level.SEVERE, "Error while trying to reload config" + e);
             commandSender.sendMessage(plugin.getMessageManager().format(Message.CONFIG_RELOAD_ERROR));
