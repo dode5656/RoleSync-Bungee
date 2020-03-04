b7d07a3691f6448b2f59394bb30ccf7f72b77f99
dode5656 <dev.dode5656@gmail.com>
2020-03-04 16:44:38 +0400

    Fix Stupid mistake

--- src/main/java/io/github/dode5656/rolesync/RoleSync.java
+++ src/main/java/io/github/dode5656/rolesync/RoleSync.java
@@ -40,6 +40,7 @@ public class RoleSync extends Plugin {
         messages.saveDefaults(this);
         messageManager = new MessageManager(this);
 
+        configChecker = new ConfigChecker(this);
         configChecker.checkDefaults();
 
         startBot();
